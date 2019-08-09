f5a3adfb468a651c856477bf9c21c24846c2eae7
user2 <user2@KH_C>
2019-08-09 20:31:37 +0900

    adasdasd

--- src/com/kh/gitTest/HelloWorld2.java
+++ src/com/kh/gitTest/HelloWorld2.java
@@ -3,7 +3,7 @@ package com.kh.gitTest;
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Worlasdasdd!");
 
 	}
 
